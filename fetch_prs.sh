c477ccb8c04e0762a8023b1b3ccd8c89df5028a3
Matthias Wientapper <m.wientapper@gmx.de>
2026-01-21 09:54:56 +0100

    Add scripts to help automate the fw build process

---
+++ fetch_prs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git branch -D pr-1199
+git branch -D pr-1297
+git branch -D pr-1338
+
+# fetch PRs
+git fetch upstream pull/1338/head:pr-1338
+git fetch upstream pull/1338/head:pr-1297
+git fetch upstream pull/1199/head:pr-1199
