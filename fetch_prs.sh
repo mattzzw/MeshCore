ce2c321a07072819dd046fc895e8e695ff1ed639
Matthias Wientapper <m.wientapper@gmx.de>
2026-02-09 19:34:16 +0100

    latest evo build updates

--- fetch_prs.sh
+++ fetch_prs.sh
@@ -2,7 +2,9 @@
 
 git branch -D pr-1297
 git branch -D pr-1338
+git branch -D pr-1398
 
 # fetch PRs
+git fetch upstream pull/1398/head:pr-1398
 git fetch upstream pull/1338/head:pr-1338
 git fetch upstream pull/1297/head:pr-1297
